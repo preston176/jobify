3a9b02a84e3e28eb1e6b3056eb11361bf5138b86
preston176 <prestonnyamweya@gmail.com>
2023-12-12 17:40:28 +0300

    Error element added

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <HomeLayout />,
-    errorElement: <Error />,
+    errorElement: <Error />, 
     children: [
       {
         index: true,
