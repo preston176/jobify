96209a7a8b198d53f7a2601fecd5ad1653fd2066
preston176 <prestonnyamweya@gmail.com>
2023-12-13 22:19:25 +0300

    Add dev script and update server.js

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
-import { value } from './test.js';
-console.log(value);
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    console.log(req);
+
+    res.json({ message: 'Data received', data: req.body });
+});
+
+app.listen(5100, () => {
+    console.log('server running.... on 5100');
+});
