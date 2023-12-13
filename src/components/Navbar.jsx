7889ef305b2a73566da45ceaca6acc6fa12acfbe
preston176 <prestonnyamweya@gmail.com>
2023-12-13 21:55:46 +0300

    Add ThemeToggle component to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,26 @@ import { FaAlignLeft } from 'react-icons/fa';
 import Logo from './Logo';
 
 import { useDashboardContext } from '../pages/DashboardLayout';
+import ThemeToggle from './ThemeToggle';
 const Navbar = () => {
-  const { toggleSidebar } = useDashboardContext();
-  return (
-    <Wrapper>
-      <div className='nav-center'>
-        <button type='button' className='toggle-btn' onClick={toggleSidebar}>
-          <FaAlignLeft />
-        </button>
-        <div>
-          <Logo />
-          <h4 className='logo-text'>dashboard</h4>
-        </div>
-        <div className='btn-container'>toggle/logout</div>
-      </div>
-    </Wrapper>
-  );
+    const { toggleSidebar } = useDashboardContext();
+    return (
+        <Wrapper>
+            <div className='nav-center'>
+                <button type='button' className='toggle-btn' onClick={toggleSidebar}>
+                    <FaAlignLeft />
+                </button>
+                <div>
+                    <Logo />
+                    <h4 className='logo-text'>dashboard</h4>
+                </div>
+                <div className='btn-container'>
+                    <ThemeToggle />
+                </div>
+            </div>
+        </Wrapper>
+
+    );
 };
 
 export default Navbar;
