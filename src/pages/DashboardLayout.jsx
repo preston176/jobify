9c6460aee01d3e15eae3ba5c3c0add0b2acdebd7
preston176 <prestonnyamweya@gmail.com>
2023-12-13 21:22:25 +0300

    Refactor DashboardLayout component

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,10 +1,23 @@
 import { Outlet } from 'react-router-dom';
 
+import Wrapper from '../assets/wrappers/Dashboard';
+import { Navbar, BigSidebar, SmallSidebar } from '../components';
+
 const DashboardLayout = () => {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <Wrapper>
+      <main className='dashboard'>
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className='dashboard-page'>
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
   );
 };
+
 export default DashboardLayout;
