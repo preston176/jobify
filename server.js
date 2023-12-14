7cffb1f857697032548c130915a9a09b4728e1bb
preston176 <prestonnyamweya@gmail.com>
2023-12-14 21:44:55 +0300

    Add error middleware to handle server errors

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.use('*', (req, res) => {
     res.status(404).json({ msg: 'not found' });
 });
 
+//ERROR MIDDLEWARE
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({ msg: 'something went wrong' });
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
