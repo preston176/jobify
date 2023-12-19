59a4069758a63fbfe32f9411f588d25dce71d9fc
preston176 <prestonnyamweya@gmail.com>
2023-12-19 05:02:39 +0300

    cors added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 const app = express();
+const cors = require('cors');
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
@@ -24,28 +25,29 @@ import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 import { authenticateUser } from './middleware/authMiddleware.js';
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+    app.use(morgan('dev'));
 }
 app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
 app.use(mongoSanitize());
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
 
 app.get('/api/v1/test', (req, res) => {
-  res.json({ msg: 'test route' });
+    res.json({ msg: 'test route' });
 });
 
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
@@ -53,11 +55,11 @@ app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 app.use('*', (req, res) => {
-  res.status(404).json({ msg: 'not found' });
+    res.status(404).json({ msg: 'not found' });
 });
 
 app.use(errorHandlerMiddleware);
@@ -65,11 +67,11 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5100;
 
 try {
-  await mongoose.connect(process.env.MONGO_URL);
-  app.listen(port, () => {
-    console.log(`server running on PORT ${port}...`);
-  });
+    await mongoose.connect(process.env.MONGO_URL);
+    app.listen(port, () => {
+        console.log(`server running on PORT ${port}...`);
+    });
 } catch (error) {
-  console.log(error);
-  process.exit(1);
+    console.log(error);
+    process.exit(1);
 }
